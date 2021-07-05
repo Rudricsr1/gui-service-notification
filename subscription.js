abf73131c1fdd39fb40762ced71dfded6afbd5a1
rcs <rcs@accurate.com>
2021-07-05 23:42:34 +0530

    Added css styles and other changes

--- subscription.js
+++ subscription.js
@@ -19,10 +19,10 @@ function updateSubscription(subscriptionStatusRequest, notificationref, token) {
     contentType: "application/json;",
     success: function (data, ref, tok) {
       console.log("success");
+      window.location.reload();
     },
     error: function () {
       console.log("error for the url : ");
-      alert("Exception while updating");
     },
   });
 }
@@ -30,23 +30,85 @@ function updateSubscription(subscriptionStatusRequest, notificationref, token) {
 function change(el) {
   console.log("Inside change");
   console.log("el = " + el.content);
+  console.log("The URL of this page is: " + window.location.href);
   var subscriptionStatusRequest = new SubscriptionStatusRequest();
   if (el.value === "Opt back in") {
     el.value = "Opt back out";
+    isSubscribed = true;
+    document.getElementById("opt-out").hidden = true;
     subscriptionStatusRequest.status = "SUBSCRIBED";
   } else {
     el.value = "Opt back in";
     subscriptionStatusRequest.status = "UNSUBSCRIBED";
+    document.getElementById("opt-in").hidden = true;
   }
 
   console.log(
     "SubscriptionStatusRequest status is : " + subscriptionStatusRequest.status
   );
 
-  var token = "abcdefgh";
-  var encodedStringAtoB = "KzM0NjY2MTE1NjA3";
+  var splitUrl = window.location.href.split("/");
+
+  var token = splitUrl[splitUrl.length - 1];
+  console.log("token is: " + token);
+
+  var encodedStringAtoB = splitUrl[splitUrl.length - 2];
+  console.log("encodedStringAtoB is: " + encodedStringAtoB);
   var notificationref = atob(encodedStringAtoB);
-  console.log("token is : " + token);
-  console.log("notificationref is : " + notificationref);
+
   updateSubscription(subscriptionStatusRequest, notificationref, token);
 }
+
+
+
+window.onload = function exampleFunction() {
+  console.log("The page is loading");
+
+  var splitUrl = window.location.href.split("/");
+  var encodedStringAtoB = splitUrl[splitUrl.length - 2];
+  console.log("encodedStringAtoB is: " + encodedStringAtoB);
+  var phoneNumber = atob(encodedStringAtoB);
+
+  //var jsonData = { phoneNumber: phoneNumber };
+
+  $.ajax({
+    url: "http://localhost:8080/v1/sms/subscription-status",
+    type: "GET",
+    data: { phoneNumber: phoneNumber },
+    contentType: "application/json;",
+    success: function (response) {
+      console.log(response);
+      console.log(
+        "phoneNumber " + phoneNumber + " is " + response.subscriptionStatus
+      );
+
+      $("#outer-rectangle").show();
+
+      var isSubscribed = false;
+      if (response.subscriptionStatus === "SUBSCRIBED") {
+        isSubscribed = true;
+        console.log("isSubscribed : " + isSubscribed);
+        $("#opt-in").show();
+        $("#optin-message").show();
+        $("#optout-button").show();
+        $("#optin-message").text($("#optin-message").text() + phoneNumber);
+      }
+      if (response.subscriptionStatus === "UNSUBSCRIBED") {
+        console.log("isSubscribed : " + isSubscribed);
+        $("#opt-out").show();
+        $("#optout-message").show();
+        $("#optin-button").show();
+        $("#optout-message").text($("#optout-message").text() + phoneNumber);
+      }
+    },
+    error: function () {
+      console.log("error for the url : ");
+    },
+  });
+
+  function closewindow() {
+    console.log("Inside closewindow");
+    window.close();
+  }
+
+};
